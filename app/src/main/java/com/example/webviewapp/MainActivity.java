8c225fd95317a0661088933f097869d4617fe59c
samuel <a19samke@student.his.se>
2020-04-19 15:08:44 +0200

    web-gogle

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView=findViewById(R.id.my_webview);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("file:///android_asset/about.html");
+        webView.loadUrl("http://www.google.com");
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
